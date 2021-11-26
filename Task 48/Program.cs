c506c67d7a579a644aec19adf13a87884d9b8791
Sergey <s.a.pavlovskiy1987@gmail.com>
2021-11-27 00:18:23 +0500

    new

---
+++ Task 48/Program.cs
@@ -0,0 +1,12 @@
+﻿int[,] CreateArray = new int[5, 10];
+for (int i = 0; i < CreateArray.GetLength(0); i++)
+{
+    for (int j = 0; j < CreateArray.GetLength(1); j++)
+    {
+        CreateArray[i, j] = new Random().Next(1, 10);
+
+        System.Console.Write($"{CreateArray[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+
---
+++ Task 49/Program.cs
@@ -0,0 +1,11 @@
+﻿double[,] array = new double[5, 5];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().NextDouble() * 10;
+        System.Console.Write($"{array[i, j]} ");
+
+    }
+    System.Console.WriteLine();
+}
